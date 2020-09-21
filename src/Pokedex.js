85dd7c642b7c01bd21730dfa9c6b7e3f8f0f8ce6
Jeremy Hart <jeremynhart@gmail.com>
2020-09-21 16:55:44 -0600

    Added Grid Items & Logic

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,12 +1,32 @@
 import React from "react";
-import {AppBar, Toolbar} from '@material-ui/core';
+import {AppBar, Toolbar, Grid, makeStyles} from '@material-ui/core';
+
+const useStyles = makeStyles({
+    pokedexContainer: {
+    marginTop: "75px",
+    paddingTop: "20px",
+    paddingLeft: "50px",
+    paddingRight: "50px",
+}})
 
 const Pokedex = () => {
+    const classes = useStyles();
     return (
         <>
         <AppBar postion="static">
             <Toolbar></Toolbar>
         </AppBar>
+        <Grid margin-top={75} container spacing={2} className={classes.pokedexContainer}>
+            <Grid item xs={4}>
+                Item 1
+            </Grid>
+            <Grid item xs={4}>
+                Item 2
+            </Grid>
+            <Grid item xs={4}>
+                Item 3
+            </Grid>
+        </Grid>
         </>
     );
 };
