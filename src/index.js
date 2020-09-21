3bbc4812c6269166c63bb8c1ebfaf107e597edaa
Jeremy Hart <jeremynhart@gmail.com>
2020-09-21 11:52:11 -0600

    React Router Config && Dependancies Added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Router } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
+
   <React.StrictMode>
+    <Router history={history}>
     <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
