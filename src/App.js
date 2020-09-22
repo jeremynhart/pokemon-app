2469e1f3fba461fda0f1ee9dd52601f651d00a7e
Jeremy Hart <jeremynhart@gmail.com>
2020-09-22 06:10:45 -0600

    App JS Final Edit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import Pokedex from './Pokedex';
-import Pokemon from './Pokemon';
+  
+import React from "react";
+import Pokedex from "./Pokedex";
+import Pokemon from "./Pokemon";
+import { Route, Switch } from "react-router-dom";
 
-
-function App() {
-  return (
+const App = () => (
   <Switch>
-    <Route exact path="/" render={(props) => <Pokedex {...props}/>} />
-    <Route exact path="/:pokemonId" render={(props) => <Pokemon {...props}/>} />
+    <Route exact path="/" render={(props) => <Pokedex {...props} />} />
+    <Route
+      exact
+      path="/:pokemonId"
+      render={(props) => <Pokemon {...props} />}
+    />
   </Switch>
-  );
-}
+);
 
 export default App;
